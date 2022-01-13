1aa5c64bf8110916f76dc4c82cda4784239885b3
kienbachoc1 <kienbachoc1@gmail.com>
2022-01-13 10:16:50 +0700

    popupTrailer

--- src/components/RSlick/MultipleRowSlick.js
+++ src/components/RSlick/MultipleRowSlick.js
@@ -1,58 +1,63 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import { LeftOutlined, RightOutlined } from '@ant-design/icons';
-import styleSlick from './MultipleRowSlick.module.css';
-import Film from '../Film/Film'
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import styleSlick from "./MultipleRowSlick.module.css";
+import Film from "../Film/Film";
 import Film_Flip from "../Film/Film_Flip";
 import { useDispatch, useSelector } from "react-redux";
-import { SET_FILM_DANG_CHIEU, SET_FILM_SAP_CHIEU } from '../../redux/actions/types/QuanLyPhimType'
+import {
+  SET_FILM_DANG_CHIEU,
+  SET_FILM_SAP_CHIEU,
+} from "../../redux/actions/types/QuanLyPhimType";
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
-      className={`${className} ${styleSlick['slick-prev']}`}
+      className={`${className} ${styleSlick["slick-prev"]}`}
       style={{ ...style, display: "block" }}
       onClick={onClick}
-    >
-    </div>
-
+    ></div>
   );
 }
-
-
 
 function SamplePrevArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
-      className={`${className} ${styleSlick['slick-prev']}`}
-
-      style={{ ...style, display: "block", left: '-50px' }}
+      className={`${className} ${styleSlick["slick-prev"]}`}
+      style={{ ...style, display: "block", left: "-50px" }}
       onClick={onClick}
-    >
-    </div>
+    ></div>
   );
 }
 
-
 const MultipleRowSlick = (props) => {
   const dispatch = useDispatch();
-  const { dangChieu, sapChieu } = useSelector(state => state.QuanLyPhimReducer);
+  const { dangChieu, sapChieu } = useSelector(
+    (state) => state.QuanLyPhimReducer
+  );
 
   const renderFilms = () => {
+    const { openModal, setOpenModal, item, setMaPhim } = props;
 
-    return props.arrFilm.slice(0, 12).map((item, index) => {
-      return <div className="mt-2" key={index}  >
-        <Film_Flip item={item} />
-      </div>
-    })
-  }
-  let activeClassDC = dangChieu === true ? 'active_Film' : 'none_active_Film';
+    return props.arrFilm.map((item, index) => {
+      return (
+        <div className="mt-2" key={index}>
+          <Film_Flip
+            item={item}
+            setOpenModal={setOpenModal}
+            setMaPhim={setMaPhim}
+          />
+        </div>
+      );
+    });
+  };
+  let activeClassDC = dangChieu === true ? "active_Film" : "none_active_Film";
 
-  let activeClassSC = sapChieu === true ? 'active_Film' : 'none_active_Film';
+  let activeClassSC = sapChieu === true ? "active_Film" : "none_active_Film";
 
-  console.log('activeSC', activeClassSC)
+  console.log("activeSC", activeClassSC);
 
   const settings = {
     className: "center variable-width",
@@ -67,24 +72,29 @@ const MultipleRowSlick = (props) => {
     prevArrow: <SamplePrevArrow />,
   };
 
-
-
   return (
     <div>
-      <button className={`${styleSlick[activeClassDC]} px-8 py-3 font-semibold rounded bg-gray-800 text-white mr-2`} onClick={() => {
-        const action = { type: SET_FILM_DANG_CHIEU }
-        dispatch(action);
-      }}>PHIM ĐANG CHIẾU</button>
-      <button className={`${styleSlick[activeClassSC]} px-8 py-3 font-semibold rounded bg-white text-gray-800 border-gray-800 border`} onClick={() => {
-        const action = { type: SET_FILM_SAP_CHIEU }
-        dispatch(action);
-      }}>PHIM SẮP CHIẾU</button>
-      <Slider {...settings}>
-        {renderFilms()}
-      </Slider>
+      <button
+        className={`${styleSlick[activeClassDC]} px-8 py-3 font-semibold rounded bg-gray-800 text-white mr-2`}
+        onClick={() => {
+          const action = { type: SET_FILM_DANG_CHIEU };
+          dispatch(action);
+        }}
+      >
+        PHIM ĐANG CHIẾU
+      </button>
+      <button
+        className={`${styleSlick[activeClassSC]} px-8 py-3 font-semibold rounded bg-white text-gray-800 border-gray-800 border`}
+        onClick={() => {
+          const action = { type: SET_FILM_SAP_CHIEU };
+          dispatch(action);
+        }}
+      >
+        PHIM SẮP CHIẾU
+      </button>
+      <Slider {...settings}>{renderFilms()}</Slider>
     </div>
   );
-}
-
+};
 
 export default MultipleRowSlick;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,56 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import HomeMenu from './HomeMenu/HomeMenu'
+import React, { useEffect, useState } from "react";
+import HomeMenu from "./HomeMenu/HomeMenu";
 //Kết nối redux
-import { useSelector, useDispatch } from 'react-redux'
-import Film from '../../components/Film/Film';
-import MultipleRowSlick from '../../components/RSlick/MultipleRowSlick'
-import { layDanhSachPhimAction } from '../../redux/actions/QuanLyPhimActions';
-import { layDanhSachHeThongRapAction } from '../../redux/actions/QuanLyRapActions';
-import HomeCarousel from '../../templates/HomeTemplate/Layout/HomeCarousel/HomeCarousel';
+import { useSelector, useDispatch } from "react-redux";
+import Film from "../../components/Film/Film";
+import MultipleRowSlick from "../../components/RSlick/MultipleRowSlick";
+import { layDanhSachPhimAction } from "../../redux/actions/QuanLyPhimActions";
+import { layDanhSachHeThongRapAction } from "../../redux/actions/QuanLyRapActions";
+import HomeCarousel from "../../templates/HomeTemplate/Layout/HomeCarousel/HomeCarousel";
+import PopupTrailer from "../../components/Film/PopupTrailer";
 
 export default function Home(props) {
+  const { arrFilm } = useSelector((state) => state.QuanLyPhimReducer);
+  const { heThongRapChieu } = useSelector((state) => state.QuanLyRapReducer);
+  const dispatch = useDispatch();
+  console.log("propsHome", arrFilm);
 
-    const { arrFilm } = useSelector(state => state.QuanLyPhimReducer);
-    const {heThongRapChieu} = useSelector(state => state.QuanLyRapReducer);
-    const dispatch = useDispatch();
-    console.log('propsHome', arrFilm);
+  // props.match.params
+  // const renderFilms = () => {
+  //     return arrFilm.map((phim, index) => {
+  //         return <Film key={index} />
 
-    
-    // props.match.params
-    // const renderFilms = () => {
-    //     return arrFilm.map((phim, index) => {
-    //         return <Film key={index} />
+  //     })
+  // }
 
+  useEffect(() => {
+    const action = layDanhSachPhimAction();
+    dispatch(action); //dispatch function từ thunk
 
-    //     })
-    // }
+    dispatch(layDanhSachHeThongRapAction());
+  }, []);
 
-    useEffect(()=>{
-        const action = layDanhSachPhimAction();
-        dispatch(action); //dispatch function từ thunk
+  const [openModal, setOpenModal] = useState(false);
+  const [maPhim, setMaPhim] = useState(1282);
 
-        dispatch(layDanhSachHeThongRapAction());
-
-    },[])
-    
-    return (
-        <div>
-            <HomeCarousel />
-
-            <section className="text-gray-600 body-font" >
-                <div className="container px-5 py-24 mx-auto " >
-
-                    <MultipleRowSlick arrFilm={arrFilm}/>
-                    {/* <div className="flex flex-wrap  " style={{ justifyContent: 'center' }}>
+  return (
+    <div>
+      <HomeCarousel />
+      <PopupTrailer
+        openModal={openModal}
+        setOpenModal={setOpenModal}
+        arrFilm={arrFilm}
+        maPhim={maPhim}
+      />
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto ">
+          <MultipleRowSlick
+            arrFilm={arrFilm}
+            setOpenModal={setOpenModal}
+            setMaPhim={setMaPhim}
+          />
+          {/* <div className="flex flex-wrap  " style={{ justifyContent: 'center' }}>
                         {renderFilms()}
                     </div> */}
-                </div>
-            </section>
-
-            <div className="mx-36">
-                <HomeMenu  heThongRapChieu={heThongRapChieu}/>
-
-            </div>
         </div>
-    )
+      </section>
+
+      <div className="mx-36">
+        <HomeMenu
+          heThongRapChieu={heThongRapChieu}
+          setOpenModal={setOpenModal}
+        />
+      </div>
+    </div>
+  );
 }
